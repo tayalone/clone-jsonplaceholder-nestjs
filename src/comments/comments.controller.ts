74731d2fd80c0969e3005cad75af9943545a58f4
Panupong Rong-O <tayalone@gmail.com>
2022-05-25 00:05:51 +0700

    ✨ ทำ routes GET:/comments จบ

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
-import { CommentsService } from './comments.service';
+import { Controller, Get, ParseIntPipe, Query } from '@nestjs/common'
+import { CommentsService } from './comments.service'
+import { Comment } from './interfaces/comments.interfaces'
 
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
+
+  @Get()
+  findAllComment(@Query('postId') postId?: number): Comment[] {
+    const queryOptions: any = {}
+    if (postId) {
+      queryOptions.postId = postId
+    }
+    return this.commentsService.findAll({ ...queryOptions })
+  }
 }
