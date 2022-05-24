2e5530c8799bea0353d54630076f9436d960b41b
Panupong Rong-O <tayalone@gmail.com>
2022-05-24 21:26:50 +0700

    ✨ ทำ route GET :/posts เสร็จแล้ว

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,14 @@
 import { Controller, Get } from '@nestjs/common'
 import { PostsService } from './posts.service'
+import { Post } from './interfaces/post.interface'
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Get()
-  findAllPost(): string {
-    return 'This action returns all post'
+  findAllPost(): Post[] {
+    // return this.postsService.findAll()
+    return this.postsService.findAll()
   }
 }
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common'
 import { Post } from './interfaces/post.interface'
-import mockData from '../../mock/data.json'
+import { POSTS } from '../../mock/posts'
 
 @Injectable()
 export class PostsService {
-  private readonly posts: Post[] = mockData.posts
+  private readonly posts: Post[] = POSTS
 
   findAll(): Post[] {
     return this.posts
