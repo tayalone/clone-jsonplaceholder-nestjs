8c8ab83dd95ce788ea02ce0f35fd7342c48f5ecd
Panupong Rong-O <tayalone@gmail.com>
2022-05-24 23:15:46 +0700

    🎉 เริ่มสร้าง resources comments

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { PostsModule } from './posts/posts.module'
+import { CommentsModule } from './comments/comments.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [PostsModule],
+  imports: [PostsModule, CommentsModule],
 })
 export class AppModule {}
