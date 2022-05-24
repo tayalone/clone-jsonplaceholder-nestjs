eeff26be67dd4646d9aeed22a346080a4a9e2321
Panupong Rong-O <tayalone@gmail.com>
2022-05-24 20:41:17 +0700

    ✨ ลองสร้าง route GET:/posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Get } from '@nestjs/common'
 import { PostsService } from './posts.service'
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
+
+  @Get()
+  findAllPost(): string {
+    return 'This action returns all post'
+  }
 }
