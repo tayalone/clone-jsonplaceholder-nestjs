31308863f30bb409858bdde2e6cea74ef8ec55ca
Panupong Rong-O <tayalone@gmail.com>
2022-05-24 23:36:32 +0700

    ✨ สร้าง findAll ของ comments.services จบ

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { COMMENTS } from '../../mock/comments'
+import { Comment } from './interfaces/comments.interfaces'
 
 @Injectable()
-export class CommentsService {}
+export class CommentsService {
+  private readonly comments: Comment[] = COMMENTS
+
+  findAll({ postId }: { postId?: number }): Comment[] {
+    const comments = this.comments.filter((c) => {
+      return true && postId ? c.postId === postId : true
+    })
+    return comments
+  }
+}
