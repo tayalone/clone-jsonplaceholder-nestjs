665385f1f59cdf17bb524f0e00315086e74eae6b
Panupong Rong-O <tayalone@gmail.com>
2022-06-09 23:21:21 +0700

    ✨ เพิ่ม photo.entity, album.entity

--- src/albums/entities/album.entity.ts
+++ src/albums/entities/album.entity.ts
@@ -1,1 +1,17 @@
-export class Album {}
+import { Field, Int, ObjectType } from '@nestjs/graphql'
+import { User } from '@users/entities/user.entity'
+
+@ObjectType()
+export class Album {
+  @Field(() => Int, { description: 'Album Unique Id' })
+  id: number
+
+  @Field()
+  title: string
+
+  @Field()
+  url: string
+
+  @Field(() => User, { description: 'Child Comment' })
+  owner: User
+}
--- src/photos/entities/photo.entity.ts
+++ src/photos/entities/photo.entity.ts
@@ -1,1 +1,23 @@
-export class Photo {}
+import { Field, Int, ObjectType } from '@nestjs/graphql'
+import { Album } from '@albums/entities/album.entity'
+
+@ObjectType()
+export class Photo {
+  @Field(() => Int, { description: 'Photo Unique Id' })
+  id: number
+
+  @Field(() => Int, { description: 'Id of Album' })
+  albumId: number
+
+  @Field()
+  title: string
+
+  @Field()
+  url: string
+
+  @Field()
+  thumbnailUrl: string
+
+  @Field(() => Album, { description: 'Child Comment' })
+  album: Album
+}
